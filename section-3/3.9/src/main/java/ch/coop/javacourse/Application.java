d52d8f8c941c66116d4ce8e2e36b00952c7632b3
Leon Hochwimmer <leon.hochwimmer@coop.ch>
2022-02-23 14:57:29 +0100

    changed folder structure & configuration

--- section-3/3.9/src/main/java/ch/coop/javacourse/Application.java
+++ section-3/3.9/src/main/java/ch/coop/javacourse/Application.java
@@ -1,3 +1,5 @@
+package ch.coop.javacourse;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
---
+++ section-3/3.9/src/test/java/ch/coop/javacourse/ApplicationTest.java
@@ -0,0 +1,5 @@
+package ch.coop.javacourse;
+
+public class ApplicationTest {
+
+}
