a8f5dea497d91ba658d5990504e6ec95d4b4a331
Leon Hochwimmer <leon.hochwimmer@coop.ch>
2022-03-02 16:13:46 +0100

    continued

--- section-4/src/main/java/ExpressionsOperators.java
+++ section-4/src/main/java/ExpressionsOperators.java
@@ -1,5 +1,14 @@
 public class ExpressionsOperators {
     public static void main(String[] args) {
+        System.out.println(Math.random());
+        System.out.println(-Math.random());
+        System.out.println(Math.random() * 10);
+
+        System.out.println(1 < 2);
+        System.out.println(1 < 0);
+        System.out.println(1 == 1);
+        System.out.println(1 != 1);
+        System.out.println(1 != 0);
 
     }
 }
